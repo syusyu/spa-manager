16381eb965b2d1a325669b145ab83c925aa6509e
syusyu <syuduckdv@hotmail.com>
2017-09-28 11:46:47 +0900

    Reflect the website change of wdio

--- test/e2e_wdio/user_service/testSpec01.js
+++ test/e2e_wdio/user_service/testSpec01.js
@@ -3,6 +3,6 @@ describe('webdriver.io page', function() {
     it('should have the right title - the fancy generator way', function () {
         browser.url('http://webdriver.io');
         var title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
+        assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
     });
 });
